ec3dd25aa4e7ceb38bcfc180b931dd80fc79653b
Gonçalo Estrelado <goncalo.estrelado@hotmail.com>
2022-02-17 01:05:35 +0000

    changed list view directly in profile

--- routes/utils/isAnimeEqualTo.js
+++ routes/utils/isAnimeEqualTo.js
@@ -1,5 +1,3 @@
-const express = require("express");
-
 function isAnimeEqualTo(object1, object2) {
   let result;
   for (let i = 0; i < object1.length; i++) {
@@ -10,14 +8,5 @@ function isAnimeEqualTo(object1, object2) {
   }
   return result;
 }
-
-/* const test1 = [
-  { id: 1},
-  { id: 2}
-]
-
-const test2 = { id: 2 }
-
-console.log(isAnimeEqualTo(test1,test2)) */
 
 module.exports = { isAnimeEqualTo };
